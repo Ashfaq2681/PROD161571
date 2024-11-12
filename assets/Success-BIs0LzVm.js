85cc862b93d103fdde92f408e7713d1e36bc42f0
abaidullah696 <abaidullah696@gmail.com>
2024-11-13 01:18:21 +0500

    test

--- assets/Success-BIs0LzVm.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BST-I6ML.js";const t=()=>e.jsx("div",{className:"h-lvh flex flex-row justify-center items-center w-full",children:"Payment Success"});export{t as default};
